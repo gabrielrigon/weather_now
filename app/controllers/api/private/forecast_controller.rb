c4900bb85d7ddac8a53cf41b44dade2bf08de1df
Gabriel Rigon <gabrielrigon@outlook.com>
2019-05-25 10:12:59 -0300

    feat(controllers): implements 3rd party request

--- app/controllers/api/private/forecast_controller.rb
+++ app/controllers/api/private/forecast_controller.rb
@@ -1,13 +1,15 @@
 class Api::Private::ForecastController < Api::Private::BaseController
+  DEFAULT_CITY = "São Paulo"
+
   def index
+    temperature = ::ThirdParty::OpenWeatherMap.new(DEFAULT_CITY).fetch_formatted
+    return head :failed_dependency if temperature.blank?
+
     render json: {
       data: {
         forecast: {
-          city: 'São Paulo',
-          temperature: {
-            fahrenheit: nil,
-            celsius: nil
-          }
+          city: "São Paulo",
+          temperature: temperature
         }
       }
     }
