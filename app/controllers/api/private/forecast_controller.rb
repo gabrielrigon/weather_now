49cbcb9cf8bb310f6a59af77f7099a89cbc51301
Gabriel Rigon <gabrielrigon@outlook.com>
2019-05-26 17:20:58 -0300

    feat(controllers): permit to receive city as param

--- app/controllers/api/private/forecast_controller.rb
+++ app/controllers/api/private/forecast_controller.rb
@@ -1,15 +1,38 @@
 class Api::Private::ForecastController < Api::Private::BaseController
-  DEFAULT_CITY = "São Paulo"
+  include Api::Private::ForecastHelper
+
+  before_action :validate_params, only: :index
 
   def index
-    forecast = ::ThirdParty::OpenWeatherMap.new(DEFAULT_CITY).fetch_formatted
-    return head :failed_dependency if forecast.blank?
+    city = forecast_params.dig(:city)
+    result = ::ThirdParty::OpenWeatherMap.new(city).fetch_formatted
 
-    render json: {
-      payload: {
-        city: DEFAULT_CITY,
-        forecast: forecast
+    if result.dig(:status) == :success 
+      return render json: {
+        payload: {
+          city: city,
+          forecast: result.dig(:data)
+        }
       }
-    }
+    end
+
+    render_error(result.dig(:code), result.dig(:message))
+  end
+
+  protected
+
+  def forecast_params
+    params.permit(:city)
+  end
+
+  private
+  
+  def validate_params
+    return if valid_index_params?
+    render_error(:bad_request, "missing param :city")
+  end
+
+  def render_error(status, message)
+    render(status: status, json: { error: message })
   end
 end
