97fe37050ebb51e9c3e6b1e05bcd50b353c5e690
Gabriel Rigon <gabrielrigon@outlook.com>
2019-05-26 17:20:17 -0300

    feat(services): improves error handling on owm

--- app/services/third_party/open_weather_map.rb
+++ app/services/third_party/open_weather_map.rb
@@ -1,13 +1,11 @@
 class ThirdParty::OpenWeatherMap
-  URL = ENV['OPEN_WEATHER_MAP_URL']
+  URL = ENV["OPEN_WEATHER_MAP_URL"]
 
   def initialize(city)
     @city = URI.encode(city)
   end
 
   def fetch_formatted
-    return {} unless make_request.present?
-
     @response = JSON.parse(make_request)
     parsed_data
   end
@@ -18,27 +16,36 @@ class ThirdParty::OpenWeatherMap
     key = @city.parameterize
     url_with_options = "#{URL}&q=#{@city}"
 
-    Rails.cache.fetch("third_party/open_weather_map?city=#{key}", expires_in: 1.hour) do
+    Rails.cache.fetch("third_party/open_weather_map?city=#{key}", expires_in: 1.minute) do
       begin
         HTTParty.get(url_with_options).to_json
       rescue HTTParty::Error, SocketError => e
-        p "Could not retrieve data from OpenWeatherApi"
-        p e
-
-        nil
+        p "Could not retrieve data from OpenWeatherApi, connection error"
+        { cod: 424, message: "connection error" }.to_json 
       end
     end
   end
 
   def parsed_data
-    return {} if @response.dig("list").blank?
+    return handled_error(@response) if @response.dig("cod").to_i >= 300
 
     fahrenheit_temp = @response.dig("list").first.dig("main", "temp")
     celsius_temp = ::Weather::Converters::Fahrenheit.new(fahrenheit_temp).to_celsius
 
-    return {
-      fahrenheit: fahrenheit_temp.round,
-      celsius: celsius_temp
+    {
+      status: :success,
+      data: {
+        fahrenheit: fahrenheit_temp.round,
+        celsius: celsius_temp
+      }
+    }
+  end
+
+  def handled_error(response)
+    { 
+      status: :error,
+      code: response.dig("cod"),
+      message: response.dig("message")
     }
   end
 end
