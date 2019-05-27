b8971acf412163536a70fcb6d667df1bff18a7e6
Gabriel Rigon <gabrielrigon@outlook.com>
2019-05-26 23:19:26 -0300

    test(models): adds tests for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { expect(described_class.ancestors).to include ApplicationRecord }
+
+  context "validations" do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
+  context "when the attributes are correct" do
+    let(:email) { Faker::Internet.email }
+    let(:password) { Faker::Internet.password(8) }
+
+    subject { described_class.new({ email: email, password: password }) }
+    
+    it { expect(subject).to be_valid }
+  end
+
+  context "when the password is unfilled" do
+    let(:email) { Faker::Internet.email }
+    let(:password) { nil }
+
+    subject { described_class.new({ email: email, password: password }) }
+    
+    it { expect(subject).to be_invalid }
+  end
+end
